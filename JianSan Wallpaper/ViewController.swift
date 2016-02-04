e851c76d323ffb3e4f9157b561f2086284063231
6ag <admin@6ag.cn>
2016-02-04 09:38:33 +0800

    Initial Commit

---
+++ JianSan Wallpaper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JianSan Wallpaper
+//
+//  Created by jianfeng on 16/2/4.
+//  Copyright © 2016年 六阿哥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
