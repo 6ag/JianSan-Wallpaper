a0300c863de37f3a10f18f13860fe64431e38cfb
zhoujianfeng <admin@6ag.cn>
2016-04-28 02:15:45 +0800

    增加清除缓存功能

--- JianSan Wallpaper/Classes/Module/Profile/Controller/JFProfileTableViewController.swift
+++ JianSan Wallpaper/Classes/Module/Profile/Controller/JFProfileTableViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import YYWebImage
+import SVProgressHUD
 
 class JFProfileTableViewController: JFBaseTableViewController {
 
@@ -15,21 +17,25 @@ class JFProfileTableViewController: JFBaseTableViewController {
         
         self.title = "设置"
         
-        // 第一组
         let group1CellModel1 = JFSettingCellArrow(title: "我的收藏", icon: "setting_star_icon", destinationVc: JFProfileStarViewController.self)
         let group1CellModel2 = JFSettingCellArrow(title: "上传壁纸", icon: "setting_upload_icon", destinationVc: JFProfileUploadViewController.self)
         let group1 = JFSettingGroup(cells: [group1CellModel1, group1CellModel2])
         
-        // 第二组
-        let group2CellModel1 = JFSettingCellLabel(title: "清除缓存", icon: "setting_clear_icon", text: "1732.23M")
+        let group2CellModel1 = JFSettingCellLabel(title: "清除缓存", icon: "setting_clear_icon", text: "\(String(format: "%.2f", CGFloat(YYImageCache.sharedCache().diskCache.totalCost()) / 1024 / 1024))M")
         group2CellModel1.operation = { () -> Void in
-            print("清楚缓存")
+            SVProgressHUD.showWithStatus("正在清理")
+            YYImageCache.sharedCache().diskCache.removeAllObjectsWithBlock({ 
+                SVProgressHUD.showSuccessWithStatus("清理成功")
+                group2CellModel1.text = "0.00M"
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableView.reloadData()
+                })
+            })
         }
         let group2CellModel2 = JFSettingCellArrow(title: "设置帮助", icon: "setting_help_icon", destinationVc: JFProfileHelpViewController.self)
         let group2CellModel3 = JFSettingCellArrow(title: "意见反馈", icon: "setting_feedback_icon", destinationVc: JFProfileFeedbackViewController.self)
         let group2 = JFSettingGroup(cells: [group2CellModel1, group2CellModel2, group2CellModel3])
         
-        // 第三组
         let group3CellModel1 = JFSettingCellArrow(title: "推荐给好友", icon: "setting_share_icon")
         group3CellModel1.operation = { () -> Void in
             print("推荐给好友")
